ebeba01083549fa5603fc0736bf23db5c03f35b3
Sergiooliva-1 <yimioliva.sos@gmail.com>
2020-02-10 14:24:31 -0300

    primerejercicio

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//
+function dobledecadanum (listanum) {
+    let numx2=[];
+    for (let i=0; i < listanum.length; i++){
+        numx2.push(listanum[i] * 2);
+    }
+    return numx2;
+}
+console.log (dobledecadanum ([8, 4, 6 ,8])); 
+//
+
+*//let x = [1,2,3,4,5];
+function multiplicacion(lista){
+    return lista.map8
+    (num) => num*2;
+
+}*//
+
+
+
